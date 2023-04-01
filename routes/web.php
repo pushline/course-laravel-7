c1c50c98e8c622e4d8a9de0eaa351688e25ae767
Kauê Bortolatto <kauebbortolatto@gmail.com>
2023-04-01 17:36:30 -0300

    retirado regex;

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,3 @@ Route::get('/', 'HomeController@index'); // laravel 8 = Route::get('/', [\App\Ht
 Route::get('/sobre-nos', 'SobreController@index');
 
 Route::get('/contato', 'ContatoController@index');
-
-Route::get('/contato/{nome}/{categoria_id}', 
-    function (
-        string $nome = "String",
-        int $categoria_id = 1
-    ) {
-        echo "$nome - $categoria_id"; 
-    }
-)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
