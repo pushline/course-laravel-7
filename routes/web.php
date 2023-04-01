40871e4fb1080b1c175d44d554deec75aec20fa8
Kauê Bortolatto <kauebbortolatto@gmail.com>
2023-04-01 16:25:20 -0300

    basic creation of routes using actions in the controllers;

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', 'HomeController@index'); // laravel 8 = Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/sobre-nos', 'SobreController@index');
+
+Route::get('/contato', 'ContatoController@index');
