74c4782c2d06dc36b8648a8e18151078d71cd2fc
Kauê Bortolatto <kauebbortolatto@gmail.com>
2023-04-04 20:19:37 -0300

    Layout padrão

--- resources/views/app/fornecedores/index.blade.php
+++ resources/views/app/fornecedores/index.blade.php
@@ -1,94 +1,67 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.layout')            
+@section('title')
+Fornecedores
+@stop
+          
+@section('content')
 
-        <title>Laravel</title>
+    {{-- isso é um // ou /**/ no php --}}
+    {{-- @dd($fornecedores)  pr() do cake no laravel pra array --}}
 
-        <?php // asset serve como public na url ?>
-        <link href="{{ asset('css/default.css') }}" rel="stylesheet">
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            <div class="content">
-                <div class="title m-b-md">
-                    Fornecedores
-                </div>
+    @php
+    // literalmente uma tag php diferente
+    @endphp
 
-                {{-- isso é um // ou /**/ no php --}}
-                {{-- @dd($fornecedores)  pr() do cake no laravel pra array --}}
+            
+    {{-- 
+        @for($i = 0; $isset($fornecedores[$i]); $i++)
+            {{ $fornecedores[$i][var] }}
+        @endfor
+    --}}
 
-                @php
-                    // literalmente uma tag php diferente
-                @endphp
+    {{-- 
+        @php $i = 0; @endphp
+        @while(isset($fornecedores[$i]))
+            {{  }}
+            @php $i++; @endphp - tem q aumentar ao decorrer do while 
+        @endwhile
+    --}}
+    @forelse ($fornecedores as $key => $item)
+        <br>
+        Fornecedor: {{ $item['nome'] }}<br/>
+        Status: {{ $item['status'] }}<br/>
+        
+        ID: {{ $item['id'] ?? 'DEFAULT' }} 
+        {{-- ?? seria o valor default --}}
+    @empty
+        Não há fornecedores
+    @endforelse
 
-                <div class="links">
-                    <a href="{{ route('site.home') }}">Home</a>
-                    <a href="{{ route('site.sobre') }}">Sobre Nós</a>
-                    <a href="{{ route('site.contato') }}">Contato</a>
-                    <a href="{{ route('site.login') }}">Login</a>
-                    <a href="{{ route('app.produtos') }}">Produtos</a>
-                    <a href="{{ route('app.clientes') }}">Clientes</a>
-                    <a href="{{ route('app.fornecedores') }}">Fornecedores</a>
-                </div>       
-                     
-                
-                
-                {{-- 
-                    @for($i = 0; $isset($fornecedores[$i]); $i++)
-                        {{ $fornecedores[$i][var] }}
-                    @endfor
-                --}}
 
-                {{-- 
-                    @php $i = 0; @endphp
-                    @while(isset($fornecedores[$i]))
-                        {{  }}
-                        @php $i++; @endphp - tem q aumentar ao decorrer do while 
-                    @endwhile
-                --}}
-                @forelse ($fornecedores as $key => $item)
-                    <br>
-                    Fornecedor: {{ $item['nome'] }}<br/>
-                    Status: {{ $item['status'] }}<br/>
-                    
-                    ID: {{ $item['id'] ?? 'DEFAULT' }} 
-                    {{-- ?? seria o valor default --}}
-                @empty
-                    Não há fornecedores
-                @endforelse
-                
+    {{--  
+        @for($i = 0; $i < 10; $i++)
+            {{ $i }} <br/>
+        @endfor    
+    --}}    
 
-                {{--  
-                    @for($i = 0; $i < 10; $i++)
-                       {{ $i }} <br/>
-                    @endfor    
-                --}}    
-                
 
-                {{-- 
-                    @switch($var)
-                        @case('fodase')
-                            tal coisa
-                            @break
-                        @default
-                            fodase
-                    @endswitch
+    {{-- 
+        @switch($var)
+            @case('fodase')
+                tal coisa
+                @break
+            @default
+                fodase
+        @endswitch
 
-                --}}
-                
-                {{-- @unless executa se o retorno for false --}}
-                {{-- o contrario do if(!condicao) --}}
-                {{-- @unless($fornecedores[2]['status'] == 'Y') 
-                    Fornecedor inativo
-                @endunless --}}
+    --}}
 
-               
-                {{-- if isset $var = retorna true se existe/definida --}}
-                
+    {{-- @unless executa se o retorno for false --}}
+    {{-- o contrario do if(!condicao) --}}
+    {{-- @unless($fornecedores[2]['status'] == 'Y') 
+        Fornecedor inativo
+    @endunless --}}
 
-            </div>
-        </div>
-    </body>
-</html>
+
+    {{-- if isset $var = retorna true se existe/definida --}}
+@stop
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <?php // asset serve como public na url ?>
+        <link href="{{ asset('css/default.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title m-b-md">
+                    @yield('title')
+                </div>
+
+                <div class="links">
+                    <a href="{{ route('site.home') }}">Home</a>
+                    <a href="{{ route('site.sobre') }}">Sobre Nós</a>
+                    <a href="{{ route('site.contato') }}">Contato</a>
+                    <a href="{{ route('site.login') }}">Login</a>
+                    <a href="{{ route('app.produtos') }}">Produtos</a>
+                    <a href="{{ route('app.clientes') }}">Clientes</a>
+                    <a href="{{ route('app.fornecedores') }}">Fornecedores</a>
+                </div>
+
+                @yield('content')
+            </div>
+        </div>
+    </body>
+</html>
