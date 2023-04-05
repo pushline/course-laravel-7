ef06c3d2ebcf00e7e04d6b89b13a108c3c885480
Kauê Bortolatto <kauebbortolatto@gmail.com>
2023-04-04 21:49:21 -0300

    retirado dd

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -7,9 +7,6 @@ use Illuminate\Http\Request;
 class ContatoController extends Controller
 {
     public function index() {
-
-        dd($_GET);
-
         return view('site.contato.index');
     }
 }
