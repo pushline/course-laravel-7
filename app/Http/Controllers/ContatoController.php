6106d02204fb655307cb278744870eb072f7ef21
Kauê Bortolatto <kauebbortolatto@gmail.com>
2023-04-04 21:35:06 -0300

    form get

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 class ContatoController extends Controller
 {
     public function index() {
+
+        if(!empty($_GET)){
+            $template = PHP_SAPI !== 'cli' ? '<pre>%s</pre>' : "\n%s\n";
+			printf($template, print_r($_GET, true));;
+        }
+
         return view('site.contato.index');
     }
 }
